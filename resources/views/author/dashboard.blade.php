f80d9a74d54bc2374f2711853cb4f47fb786d785
Diego Rueda <diego.rueda2@gmail.com>
2019-03-07 10:44:43 -0400

    add controllers for sahboard

---
+++ resources/views/author/dashboard.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.backend.app')
+
+@section('title','Dashboard')
+
+@push('css')
+
+@endpush
+
+@section('content')
+    <div class="container-fluid">
+        <div class="block-header">
+            <h2>DASHBOARD</h2>
+        </div>
+
+       
+
+        </div>
+    </div>
+@endsection
+
+@push('js')
+    <!-- Jquery CountTo Plugin Js -->
+    <script src="{{ asset('assets/backend/plugins/jquery-countto/jquery.countTo.js') }}"></script>
+    <script src="{{ asset('assets/backend/js/pages/index.js') }}"></script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,18 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['as'=>'admin.','prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth','admin']], function (){
+	Route::get('dashboard','DashboardController@index')->name('dashboard');
+
+	 Route::resource('tag','TagController');
+});
+
+Route::group(['as'=>'author.','prefix'=>'author','namespace'=>'Author','middleware'=>['auth','author']], function (){
+	Route::get('dashboard','DashboardController@index')->name('dashboard');
+});
+
+
